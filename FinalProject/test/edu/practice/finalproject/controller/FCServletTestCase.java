72b0ee577def8852a0ad8acf741a5027f23cd640
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2021-06-28 01:37:11 +0300

    Mockito partial test for FCServlet.process added.
    Base abstract Form class made Serializable to prevent JEE bad practice:
    CWE-579: J2EE Bad Practices: Non-serializable Object Stored in Session
    http://cwe.mitre.org/data/definitions/579.html

--- FinalProject/test/edu/practice/finalproject/controller/FCServletTestCase.java
+++ FinalProject/test/edu/practice/finalproject/controller/FCServletTestCase.java
@@ -1,40 +1,90 @@
 package edu.practice.finalproject.controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import edu.practice.finalproject.controller.transition.FormDispatcher;
+import edu.practice.finalproject.view.action.Action;
+import edu.practice.finalproject.view.form.Form;
 
 import static org.mockito.Mockito.*;
 
-class FCServletTestCase {
+import java.io.IOException;
 
-	@Mock private HttpServletRequest req;
-	@Mock private HttpServletResponse resp;
+//@ExtendWith(MockitoExtension.class)
+public class FCServletTestCase {
+
+	private HttpServletRequest req = mock(HttpServletRequest.class);
+	private HttpServletResponse resp = mock(HttpServletResponse.class);
+	private RequestDispatcher dispatcher = mock(RequestDispatcher.class);
 	
 	private FCServlet servlet = new FCServlet();
 	
-	AutoCloseable mocks;
+	//private AutoCloseable mocks;
 	
-	@BeforeAll
-	void setUp() throws Exception {
-		mocks = MockitoAnnotations.openMocks(FCServletTestCase.class);
+	@BeforeEach
+	public void setUp() {
+		//mocks = MockitoAnnotations.openMocks(this);
 	}
 
-	@AfterAll
-	void tearDown() throws Exception {
-		mocks.close();
+	@AfterEach
+	public void tearDown() {
+		//mocks.close();
 	}
 
 	@Test
-	void testForInitialPage() throws ServletException {
-		when(servlet.getForm(req)).thenReturn(null);
-		servlet.doGet(req, resp);
-		verify(req,never()).getSession();
+	public void testForInitLocale() throws ServletException {
+		HttpSession session = mock(HttpSession.class);
+		when(req.getSession()).thenReturn(session);
+		when(req.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+		servlet.doPost(req, resp);
+		verify(session).setAttribute(Names.LOCALE_ATTRIBUTE, FCServlet.getDefaultLocale());
 	}
+	
+	@Test
+	public void testForClearErrorMessage() throws ServletException {
+		HttpSession session = mock(HttpSession.class);
+		when(req.getSession()).thenReturn(session);
+		when(req.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+		servlet.doGet(req, resp);
+		verify(session).removeAttribute(Names.ERROR_ATTRIBUTE);
+		verify(session).removeAttribute(Names.MESSAGE_ATTRIBUTE);
+	}
+	
+	@Test
+	public void testForNoFormAttribute() throws ServletException, IOException {
+		HttpSession session = mock(HttpSession.class);
+		when(req.getRequestDispatcher(FormDispatcher.LOGIN_FORM.getName())).thenReturn(dispatcher);
+		when(req.getSession()).thenReturn(session);
+		servlet.doGet(req, resp);
+		verify(req, atLeastOnce()).getSession();
+		verify(dispatcher).forward(req, resp);
+		verify(session).setAttribute(Names.FORM_ATTRIBUTE, FormDispatcher.LOGIN_FORM);
+	}
+
+	@Test
+	public void testForFormAttributePresentActionSucceeded() throws ServletException, IOException {
+		HttpSession session = mock(HttpSession.class);
+		when(req.getSession()).thenReturn(session);
+		Form form = mock(Form.class);
+		when(session.getAttribute(Names.FORM_ATTRIBUTE)).thenReturn(form);
+		Action action  = mock(Action.class);
+		when(form.getAction(any())).thenReturn(action);
+		Form newForm = mock(Form.class);
+		FCServlet servletMock = mock(FCServlet.class);
+		when(servletMock.getNextForm(req, form, action, true)).thenReturn(newForm);
+		when(req.getRequestDispatcher(newForm.getName())).thenReturn(dispatcher);
+		servlet.doGet(req, resp);
+		verify(req, atLeastOnce()).getSession();
+		verify(dispatcher).forward(req, resp);
+		verify(session).setAttribute(eq(Names.FORM_ATTRIBUTE), any());
+	}
+
 }
